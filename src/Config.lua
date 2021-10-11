742e43a52dd184302464a6989931876d18b1dc2f
ATrashScripter <61103371+ATrashScripter@users.noreply.github.com>
2021-10-11 05:05:16 +0300

    Add files via upload

---
+++ src/Config.lua
@@ -0,0 +1,14 @@
+local Config = {
+	DEFAULT_NOT_RELEASED_HANDLER = "ForceLoad",
+	AUTOMATICALLY_UPDATE_LEADERSTATS = true,
+	GAME_PROFILE_TEMPLATES = {
+		PlayerStats = {
+			_Key = "PlayerStats-UserId",
+			LoadOnJoin = true,
+			Gold = 0,
+			}
+		},
+	},
+}
+
+return Config
